f946db7f0cd6a213143f47bac6abb225dfe43729
Jimmy <chen.jimmym@gmail.com>
2017-10-18 10:20:29 -0700

    done

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^process_buy', views.process_buy),
+    url(r'^checkout', views.checkout),
+    url(r'^back', views.back),
 ]
--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+def process_buy(request):
+    if not 'quantity_ordered' in request.session:
+            request.session['quantity_ordered'] = 0
+    if not 'price_ordered' in request.session:
+            request.session['price_ordered'] = 0
+    if request.method == "POST":
+        if request.POST['product_id'] == 'item1':
+            price = 20
+        elif request.POST['product_id'] == 'item2':
+            price = 30
+        elif request.POST['product_id'] == 'item3':
+            price = 5
+        request.session['checkout_price'] = int(request.POST['quantity']) * price
+        request.session['quantity_ordered'] += int(request.POST['quantity'])
+        request.session['price_ordered'] += request.session['checkout_price']
+        # print '@@@price', request.session['checkout_price']
+    return redirect('/checkout')
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')
+
+def back(request):
+    return redirect('/')
